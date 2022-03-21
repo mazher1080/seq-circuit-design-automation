b66dce7e87eb9d50b05298d383bb4a4b58a06f7f
HeadJk <jake4head@outlook.com>
2022-03-20 22:12:20 -0230

    Fix: Fixed the toString unit test for state

--- src/test/java/state_table_solver/stateTable/StateTest.java
+++ src/test/java/state_table_solver/stateTable/StateTest.java
@@ -9,41 +9,41 @@ import static org.junit.Assert.*;
 
 public class StateTest {
     @Test
-    public void toStringTest() {
+    public void toEncodedStringTest() {
         State s1 = new State("Q0", "d");
 
         s1.pushBit(BitValue.HIGH);
-        assertEquals(s1.toString(), "d0");
+        assertEquals(s1.toEncodedString(), "d0");
 
         s1.pushBit(BitValue.LOW);
-        assertEquals(s1.toString(), "d1'.d0");
+        assertEquals(s1.toEncodedString(), "d1'.d0");
 
         s1.pushBit(BitValue.UNKNOWN);
-        assertEquals(s1.toString(), "d2.d1'.d0");
+        assertEquals(s1.toEncodedString(), "d2.d1'.d0");
 
         s1.popBit();
-        assertEquals(s1.toString(), "d1'.d0");
+        assertEquals(s1.toEncodedString(), "d1'.d0");
 
         s1.popBit();
-        assertEquals(s1.toString(), "d0");
+        assertEquals(s1.toEncodedString(), "d0");
 
         s1.pushBit(BitValue.LOW);
-        assertEquals(s1.toString(), "d1'.d0");
+        assertEquals(s1.toEncodedString(), "d1'.d0");
 
         s1.pushBit(BitValue.UNKNOWN);
-        assertEquals(s1.toString(), "d2.d1'.d0");
+        assertEquals(s1.toEncodedString(), "d2.d1'.d0");
 
         s1.setEncodingId("b");
-        assertEquals(s1.toString(), "b2.b1'.b0");
+        assertEquals(s1.toEncodedString(), "b2.b1'.b0");
 
         s1.popBit();
-        assertEquals(s1.toString(), "b1'.b0");
+        assertEquals(s1.toEncodedString(), "b1'.b0");
 
         s1.popBit();
-        assertEquals(s1.toString(), "b0");
+        assertEquals(s1.toEncodedString(), "b0");
 
         s1.popBit();
-        assertEquals(s1.toString(), "");
+        assertEquals(s1.toEncodedString(), "");
     }
 
     @Test
